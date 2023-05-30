5ac16e5ff2830ace0cc7516f489b99ec19b12caf
LiJie <opwinopwin@gmail.com>
2023-05-30 08:18:04 -0400

    Order form init send to backend

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
+import '../../css/Order.css'
 
-
+const SERVER_URL= import.meta.env.VITE_APP_SERVER_URL;
 
 export function Order(){
+    
     const [text, setText] = useState('');
     const [data, setData] = useState('');
+    const [item1Value, setItem1Value]=useState(0);
     const handleChange = (event) => {
         setText(event.target.value);
     }
     async function handleSubmit(e){
         e.preventDefault();
+        // alert("You submitted it!");
         const formData={
             input:text,
+            item1_amount:item1Value
         }
-        console.log(text);
 
         let response;
         let fromServerData;
         try{
-
-            response=await fetch("http://localhost:3000/order/submit",{
+            response=await fetch(`${SERVER_URL}/order/submit`,{
                 method:"POST",
                 headers:{
                     'Content-Type':'application/json'
@@ -30,19 +33,34 @@ export function Order(){
         }catch(e){
             console.log(e);
         }
-            setData(fromServerData.Message);
-            
+            setData(fromServerData.Message);            
     }
-
+    function handleAdd(){
+        if(item1Value>=99)return;
+        setItem1Value(item1Value+1);
+    }
+    function handleSubtract(){
+        if(item1Value<=0)return;
+        setItem1Value(item1Value-1);
+    }
 
     return(
         <div className="header-padding">
-            <form action='http://localhost:3000/order/submit' method="POST" onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="test_input">Test input</label>
-                <input type="text" id="test_input" name="testform" placeholder="some text" value={text} onChange={handleChange}/>
+                <input type="text" id="test_input" placeholder="some text" value={text} onChange={handleChange}/>
+                <div className='item1'>
+                    <label htmlFor='item1'>Item 1</label>
+                    <button type="button" onClick={handleAdd}> + </button>
+                    <input id="item1" type="text"  value={item1Value} readOnly/>
+                    <button type="button" onClick={handleSubtract}> - </button>
+                </div>
                 <br/><button type="submit">Submit</button>
             </form>
-            {<h1>{data.input}</h1>||"No DATA"}
+            {<h1>{data.item1_amount}</h1>||"No DATA"}
+
+
+
         </div>
     );}
 
